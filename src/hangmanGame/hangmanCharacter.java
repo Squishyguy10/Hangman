97a9b4d69490ac093f2c33d71844307012780174
epike1 <163864944+epike1@users.noreply.github.com>
2024-03-26 09:46:14 -0400

    Update HangmanCharacter.java

--- src/hangmanGame/hangmanCharacter.java
+++ src/hangmanGame/hangmanCharacter.java
@@ -1,21 +1,46 @@
 package hangmanGame;
+import javax.imageio.*;
 import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
 
 public class HangmanCharacter extends JPanel {
 
     private static final String[] hangmanImageLocations = {
-            "src/resources/hangman0",
-            "src/resources/hangman1",
-            "src/resources/hangman2",
-            "src/resources/hangman3",
-            "src/resources/hangman4",
-            "src/resources/hangman5",
-            "src/resources/hangman6"};
+            "src/resources/hangman0.png",
+            "src/resources/hangman1.png",
+            "src/resources/hangman2.png",
+            "src/resources/hangman3.png",
+            "src/resources/hangman4.png",
+            "src/resources/hangman5.png",
+            "src/resources/hangman6.png"};
 
     private static int penalties = 0;
-    private static JLabel hangmanImage = new JLabel(new ImageIcon(hangmanImageLocations[0]));;
+    private static JLabel hangmanImage;
+
     public HangmanCharacter() {
 
+        hangmanImage = new JLabel(new ImageIcon(getCurrentImage()));
+        hangmanImage.setSize(100, 100);
+
+    }
+
+    private Image getCurrentImage() {
+
+        BufferedImage image = null;
+
+        try {
+
+            image = ImageIO.read(new File(hangmanImageLocations[penalties]));
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return image.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
     }
     public int getPenalties() {
         return penalties;
@@ -24,7 +49,8 @@ public class HangmanCharacter extends JPanel {
     public void incrementPenalties() {
         penalties += 1;
 
-        hangmanImage.setIcon(new ImageIcon(hangmanImageLocations[penalties]));
+        hangmanImage.setIcon(new ImageIcon(getCurrentImage()));
+        hangmanImage.repaint();
     }
 
     public JPanel getHangmanImage() {
